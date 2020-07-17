9d8425a9ca87aae2df0fb9f9f8d92019b8ee84f7
Parthtrrjob <parthtrrjob@gmail.com>
2020-07-17 13:06:11 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+
+
+// module.exports.actionName = function(req,res){.....}
+module.exports.home = function(req,res){
+    return res.end('<h1>Express is for kashti</h1>');
+}
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express =require('express');
 const app=express();
 const port=8000;
 
+//use express router
+app.use('/',require('./routes/index'))
+
+
 app.listen(port,function(err){
     if(err){
         console.log('Error',err);
         return;
     }
     console.log('Server running on port',port);
-})
+});
