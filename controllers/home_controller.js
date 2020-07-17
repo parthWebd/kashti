1f1b687812adc15ab96b0558054b7f71e42839fd
Parthtrrjob <parthtrrjob@gmail.com>
2020-07-17 14:29:00 +0530

    views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,5 +2,7 @@
 
 // module.exports.actionName = function(req,res){.....}
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is for kashti</h1>');
+    return res.render('home',{
+        title:'Kashti',
+    });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile=function(req,res){
-    res.end('<h1>profile</h1>');
+    res.render('user',{
+        title  :  "kashti-Users"
+    });
 };
 module.exports.friends=function(req,res){
     res.end('<h1>Friends</h1>');
